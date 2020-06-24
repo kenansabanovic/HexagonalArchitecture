04e8d64a2a088c5dcf6f38cc1ec908fa7b8155f6
Kenan Šabanović <kenan.sabanovic@tacta.io>
2020-06-24 14:11:00 +0200

    Project dependency / Category model

--- Infrastructure/DataContext.cs
+++ Infrastructure/DataContext.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Infrastructure
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
-        public DataContext()
+        public DataContext(DbContextOptions options) : base(options)
         {
 
         }
+
+        public DbSet<Category> Categories { get; set; }
+
+   
     }
 }
